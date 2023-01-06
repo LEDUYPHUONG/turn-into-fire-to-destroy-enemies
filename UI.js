177dbec198368c00de92f7ac247e8142ac0147db
LEDUYPHUONG <boyfriend7777777@gmail.com>
2023-01-06 16:14:55 +0700

    fix(layout): add decription, maxTime and increase amoun maxtime and win condition

--- UI.js
+++ UI.js
@@ -18,7 +18,10 @@ export class UI {
         context.fillText('Score: ' + this.game.score, 20, 50);
         // timer
         context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
-        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1) + '/' + (this.game.maxTime * 0.001), 20, 80);
+         // describe
+         context.font = this.fontSize * 0.5 + 'px ' + this.fontFamily;
+         context.fillText('Description: Use right, left, up, down keys to move; Enter key to turn fire and destroy enemies. Try to kill 100 enemies in 60s, you will win!', 20, 20);
         //lives
         for (let i = 0; i < this.game.lives; i++){
             context.drawImage(this.livesImage, 25 * i + 20, 95, 25, 25);
@@ -28,13 +31,13 @@ export class UI {
             context.textAlign = 'center';
             context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
             if(this.game.score > this.game.winningScore){
-                context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.fillText('Congratulation!!!', this.game.width * 0.5, this.game.height * 0.5 - 20);
                 context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
-                context.fillText('What are creatures of the night afraid of? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+                context.fillText('We WIN!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
             } else {
-                context.fillText('Love at first bite?', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.fillText('Iam Sorry!!!', this.game.width * 0.5, this.game.height * 0.5 - 20);
                 context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
-                context.fillText('Nope. Better luck next time!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+                context.fillText('Lets try again!', this.game.width * 0.5, this.game.height * 0.5 + 20);
             }
         }
         context.restore();
